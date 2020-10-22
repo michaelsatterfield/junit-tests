73f886eb04733044b13c8d81ea200b82c4fca50c
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-22 13:16:45 -0500

    junit exercises part 1

--- src/main/Student.java
+++ src/main/Student.java
@@ -5,14 +5,34 @@ public class Student {
     private String name;
     private ArrayList<Integer> grades;
 
-    public Student(Long _id, String _name,ArrayList<Integer> _grades){
+    public Student(Long _id, String _name){
     this.id = _id;
     this.name = _name;
-    this.grades = _grades;
-
-
+    this.grades = new ArrayList<>();
 
     }
+    public long getId(){
+        return this.id;
+    }
 
+    public String getName(){
+        return this.name;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return this.grades;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        double sum = 0;
+        for (int grade: this.grades) {
+            sum += grade;
+        }
+        return sum / this.grades.size() ;
+    }
 
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,17 +1,33 @@
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 public class StudentTest {
     @Test
     public void studentClassHasConstructor(){
-        Student jon = new Student("1","John", ArrayList<Integer>){
-
-
-
+        Student jon = new Student(1l,"jon");
+        Student nick = new Student(2l,"nick");
+        assertNotNull(jon);
+        assertNotNull(nick);
 
 }
+ @Test
+public void testStudentFields(){
+     Student jon = new Student(1l,"jon");
+     assertSame(1L,jon.getId());
+     assertSame("jon",jon.getName());
+     assertSame(0,jon.getGrades().size());
+ }
 
+ @Test
+ public void testAverage(){
+     Student jon = new Student(1l,"jon");
+     jon.addGrade(100);
+     jon.addGrade(80);
+     assertEquals(90,jon.getGradeAverage(),0);
+
+
+ }
 
 
 }
