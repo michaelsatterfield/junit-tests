2159c3adbb87342e6a0ff11c1237314b1afe63c3
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-22 10:52:50 -0500

    first instance of testing

---
+++ src/main/Student.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Student {
+    private Long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(Long _id, String _name,ArrayList<Integer> _grades){
+    this.id = _id;
+    this.name = _name;
+    this.grades = _grades;
+
+
+
+    }
+
+
+}
---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class StudentTest {
+    @Test
+    public void studentClassHasConstructor(){
+        Student jon = new Student("1","John", ArrayList<Integer>){
+
+
+
+
+}
+
+
+
+}
